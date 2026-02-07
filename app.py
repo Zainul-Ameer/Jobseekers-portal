d751dae4256bfbd9084da5dd6c770dc7de4572ef
Zainul-Ameer <za378r@gmail.com>
2026-02-07 15:53:48 +0530

    f

--- app.py
+++ app.py
@@ -12,5 +12,7 @@ if __name__ == "__main__":
     app.run(debug=True)
     
     
-# this code sets up a basic Flask application that listens for requests on the root URL ("/") and responds by rendering an HTML template called "index.html". The application is run in debug mode, which allows for easier development and troubleshooting.
-    
+# this code sets up a basic Flask application that listens for requests on the root URL ("/") and responds by rendering an HTML template called "index.html". The application is run in debug mode, which allows for easier development and troubleshooting
+
+# To run this application, you would need to have Flask installed in your Python environment. You can install it using pip:
+# pip install Flask 
